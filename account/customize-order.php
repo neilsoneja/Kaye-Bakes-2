06f8351d1936b357bdb05ba036711d21f5370f91
neilsoneja <93364375+neilsoneja@users.noreply.github.com>
2022-05-15 04:33:45 +0800

    customize page

---
+++ account/customize-order.php
@@ -0,0 +1,351 @@
+<?php
+	?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="msapplication-tap-highlight" content="no">
+  <title>Provide Order Details</title>
+
+  <!-- Favicons-->
+  <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
+  <!-- Favicons-->
+  <link rel="apple-touch-icon-precomposed" href="images/favicon/apple-touch-icon-152x152.png">
+  <!-- For iPhone -->
+  <meta name="msapplication-TileColor" content="#00bcd4">
+  <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
+  <!-- For Windows Phone -->
+
+
+  <!-- CORE CSS-->
+  <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="css/style.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- Custome CSS-->    
+  <link href="css/custom/custom.min.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link href="css/drag-drop.css" type="text/css" rel="stylesheet" >
+
+  <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+  <link href="js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
+
+    <style type="text/css">
+  .input-field div.error{
+    position: relative;
+    top: -1rem;
+    left: 0rem;
+    font-size: 0.8rem;
+    color:#FF4081;
+    -webkit-transform: translateY(0%);
+    -ms-transform: translateY(0%);
+    -o-transform: translateY(0%);
+    transform: translateY(0%);
+  }
+  .input-field label.active{
+      width:100%;
+  }
+  .left-alert input[type=text] + label:after, 
+  .left-alert input[type=password] + label:after, 
+  .left-alert input[type=email] + label:after, 
+  .left-alert input[type=url] + label:after, 
+  .left-alert input[type=time] + label:after,
+  .left-alert input[type=date] + label:after, 
+  .left-alert input[type=datetime-local] + label:after, 
+  .left-alert input[type=tel] + label:after, 
+  .left-alert input[type=number] + label:after, 
+  .left-alert input[type=search] + label:after, 
+  .left-alert textarea.materialize-textarea + label:after{
+      left:0px;
+  }
+  .right-alert input[type=text] + label:after, 
+  .right-alert input[type=password] + label:after, 
+  .right-alert input[type=email] + label:after, 
+  .right-alert input[type=url] + label:after, 
+  .right-alert input[type=time] + label:after,
+  .right-alert input[type=date] + label:after, 
+  .right-alert input[type=datetime-local] + label:after, 
+  .right-alert input[type=tel] + label:after, 
+  .right-alert input[type=number] + label:after, 
+  .right-alert input[type=search] + label:after, 
+  .right-alert textarea.materialize-textarea + label:after{
+      right:70px;
+  }
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    padding: 10px;
+}
+
+.portrait {
+    height: 64px;
+    
+}
+.center{
+    margin:auto;
+}
+
+  </style>
+</head>
+
+<body>
+  <!-- Start Page Loading -->
+  <div id="loader-wrapper">
+      <div id="loader"></div>        
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+  </div>
+  <!-- End Page Loading -->
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  <!-- START HEADER -->
+  <header id="header" class="page-topbar">
+        <!-- start header nav-->
+        <div class="navbar-fixed">
+            <nav class="navbar-color">
+                <div class="nav-wrapper">
+                    <a href="/Kaye-Bakes-2/index.html" class="portrait darken-1"><img src="images/materialize-logo.png" alt="logo"></a> <span class="logo-text"></span>
+                    
+                    <!--
+                    <ul class="right hide-on-med-and-down">                        
+                        <li><a href="#" class="waves-effect waves-block waves-light"><i class="mdi-editor-attach-money">Balance</i></a>
+                        </li>
+                    </ul>
+                    -->
+                </div>
+            </nav>
+        </div>
+        <!-- end header nav-->
+  </header>
+  <!-- END HEADER -->
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  <!-- START MAIN -->
+  <div id="main">
+    <!-- START WRAPPER -->
+    <div class="wrapper">
+
+      <!-- START LEFT SIDEBAR NAV
+      <aside id="left-sidebar-nav">
+            <ul id="slide-out" class="side-nav fixed leftside-navigation">
+                <li class="user-details cyan darken-2">
+                    <div class="row">
+                        <div class="col col s4 m4 l4">
+                            <img src="images/avatar.jpg" alt="" class="circle responsive-img valign profile-image">
+                        </div>
+                        <div class="col col s8 m8 l8">
+                            <ul id="profile-dropdown" class="dropdown-content">
+                                <li><a href="routers/logout.php"><i class="mdi-hardware-keyboard-tab"></i> Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col col s8 m8 l8">
+                            <a class="btn-flat dropdown-button waves-effect waves-light white-text profile-btn" href="#" data-activates="profile-dropdown">Name <i class="mdi-navigation-arrow-drop-down right"></i></a>
+                            <p class="user-roal">Role</p>
+                        </div>
+                    </div>
+                </li>
+                <li class="bold"><a href="index.php" class="waves-effect waves-cyan"><i class="mdi-editor-border-color"></i> Order Food</a>
+                </li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                    <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="mdi-editor-insert-invitation"></i> Orders</a>
+                        <div class="collapsible-body">
+                        <ul>
+                                            <li><a href="orders.php">All Orders</a>
+                            </li>
+                            <li><a>orders here</a>
+                            </li>
+                        </ul>
+                        </div>
+                    </li>
+                    </ul>
+                </li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                    <li class="bold"><a class="collapsible-header waves-effect waves-cyan"><i class="mdi-action-question-answer"></i> Tickets</a>
+                    <div class="collapsible-body">
+                        <ul>
+                            <li><a href="tickets.php">All Tickets</a>
+                            </li>
+                            <a>Tickets here?</a>
+                        </ul>
+                        </div>
+                    </li>
+                    </ul>
+                </li>				
+                <li class="bold"><a href="details.php" class="waves-effect waves-cyan"><i class="mdi-social-person"></i> Edit Details</a>
+                </li>				
+            </ul>
+            <a href="#" data-activates="slide-out" class="sidebar-collapse btn-floating btn-medium waves-effect waves-light hide-on-large-only cyan"><i class="mdi-navigation-menu"></i></a>
+      </aside>-->
+      <!-- END LEFT SIDEBAR NAV-->
+
+      <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+      <!-- START CONTENT -->
+      <section id="content">
+
+            <!--breadcrumbs start-->
+            <div id="breadcrumbs-wrapper">
+                <div class="container">
+                    <div class="row">
+                        <div class="col s12 m12 l12">
+                        <h5 class="breadcrumbs-title">Provide Order Details</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--breadcrumbs end-->
+
+
+            <!--start container-->
+            <div class="container">
+                <p class="caption">Provide Image for Inspiration</p>
+                <div class="divider"></div>
+                <div class="row">
+
+                    <form  id="inquery_img" method="post" action="confirm-order.php" novalidate="novalidate">
+                        <div class="drag-area">
+                            <div class="icon"><i class="fas fa-cloud-upload-alt"></i></div>
+                            <header>Drag & Drop to Upload File</header>
+                            <span>OR</span>
+                            <button>Browse File</button>
+                            <span></span>
+                            <span></span>
+                            <input type="file" hidden>
+                        </div>
+                        <span>
+                            <button class="btn " type="submit" name="action">Submit
+                                <i class="mdi-content-send right"></i>
+                            </button>
+                        </span>                              
+                            <?php
+
+                            ?>
+                    </form>
+
+                
+                </div>
+
+
+
+
+            <!--end container-->
+
+            <div class="divider"></div>
+                    
+        </section>
+      <!-- END CONTENT -->
+    </div>
+    <!-- END WRAPPER -->
+
+  </div>
+  <!-- END MAIN -->
+
+
+
+  <!-- //////////////////////////////////////////////////////////////////////////// -->
+
+  <!-- START FOOTER -->
+  <footer class="page-footer">
+    <div class="footer-copyright">
+      <div class="container">
+        <span>Copyright © 2017 <a class="grey-text text-lighten-4" href="#" target="_blank">Students</a> All rights reserved.</span>
+        <span class="right"> Design and Developed by <a class="grey-text text-lighten-4" href="#">Students</a></span>
+        </div>
+    </div>
+  </footer>
+    <!-- END FOOTER -->
+
+
+
+    <!-- ================================================
+    Scripts
+    ================================================ -->
+    
+    <!-- jQuery Library -->
+    <script type="text/javascript" src="js/plugins/jquery-1.11.2.min.js"></script>    
+    <!--drag and drop-->
+    <script type="text/javascript" src="js/drag-drop.js"></script>
+    <!--angularjs-->
+    <script type="text/javascript" src="js/plugins/angular.min.js"></script>
+    <!--materialize js-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <!--scrollbar-->
+    <script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jquery-validation/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="js/plugins/jquery-validation/additional-methods.min.js"></script>	
+	<script type="text/javascript" src="js/plugins/formatter/jquery.formatter.min.js"></script>   
+    <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+    <script type="text/javascript" src="js/plugins.min.js"></script>
+    <!--custom-script.js - Add your own theme custom JS-->
+    <script type="text/javascript" src="js/custom-script.js"></script>
+	<script type="text/javascript">
+    $("#formValidate").validate({
+        rules: {
+            address: {
+                required: true,
+                minlength: 5
+            },
+            cc_number: {
+                required: true,
+                minlength: 16,
+            },
+            cvv_number: {
+                required: true,
+                minlength: 3,
+			},
+		},
+        messages: {
+           address:{
+                required: "Enter a address",
+                minlength: "Enter at least 5 characters"
+            },	
+           cc_number:{
+                required: "Please provide card number",
+                minlength: "Enter at least 16 digits",
+            },	
+           cvv_number:{
+                required: "Please provide CVV number",
+                minlength: "Enter at least 3 digits",		
+            },				
+		},
+        errorElement : 'div',
+        errorPlacement: function(error, element) {
+          var placement = $(element).data('error');
+          if (placement) {
+            $(placement).append(error)
+          } else {
+            error.insertAfter(element);
+          }
+        }
+     });
+	  $('#cc_number').formatter({
+          'pattern': '{{9999}}-{{9999}}-{{9999}}-{{9999}}',
+          'persistent': true
+      });
+	  $('#cvv_number').formatter({
+          'pattern': '{{9}}-{{9}}-{{9}}',
+          'persistent': true
+      });
+		$('#payment_type').change(function() {
+		if ($(this).val() === 'Cash On Delivery') {
+		  $("#cc_number").prop('disabled', true);
+		  $("#cvv_number").prop('disabled', true);		  
+		}
+		if ($(this).val() === 'Wallet'){
+		  $("#cc_number").prop('disabled', false);
+		  $("#cvv_number").prop('disabled', false);	
+		}
+		});
+    </script>
+</body>
+
+</html>
+<?php
+
+?>
