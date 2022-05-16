75f7712b8c7369ad185784d3f15578091a07ef5c
NatesDGreat <88729148+NatesDGreat@users.noreply.github.com>
2022-05-16 18:20:15 +0800

    Update Front

--- account/Front.php
+++ account/Front.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Bootstrap Datetimepicker - Add Date-Time Picker to Input Field by SemicolonWorld</title>
 <!-- Minified Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -13,8 +15,6 @@
 <link rel="stylesheet" href="css/custom.min.css">
 <link href="css/bootstrap/bootstrap-datetimepicker.min.css" rel="stylesheet">
 <script src="js/date-and-time/bootstrap-datetimepicker.min.js"></script>
-
-
 </head>
 <body>
 
@@ -52,21 +52,24 @@ if(isset($_POST['submit']) && !empty($_POST['event_name']) && !empty($_POST['eve
     Date & Time: <input size="16" type="text" name="event_datetime" class="form-control" id="form_datetime" readonly>
 
 	<h3>Select Location</h3>
-	District: <select name="district" id="district">
-    	<option value="" selected="selected">Select district</option>
-  		</select>
-  		<br><br>
-  		Municipality: <select name="municipality" id="municipality">
-    	<option value="" selected="selected">Please select district first</option>
-  		</select>
-  		<br><br>
-  		City: <select name="city" id="city">
-    	<option value="" selected="selected">Please select municipality first</option>
-  		</select>
-  		<br><br>
-  		<input type="submit" value="Submit">
+	Province: <select name="province" id="province">
+    <option value="" selected="selected">Select province</option>
+  	</select>
+  	<br><br>
+
+  	Municipality: <select name="municipality" id="municipality">
+    <option value="" selected="selected">Please select district first</option>
+  	</select>
+  	<br><br>
+
+  	City: <select name="city" id="city">
+    <option value="" selected="selected">Please select municipality first</option>
+  	</select>
+  	<br><br>
+  		
+	<input type="submit" value="Submit">
 </form>
-</form>
+
 <script type="text/javascript">
 $(function () {
     var today = new Date();
@@ -81,6 +84,49 @@ $(function () {
     });
 });
 </script>
+
+<script>
+var subjectObject = {
+  "Metro Manila": {
+    "HTML": ["Links", "Images", "Tables", "Lists"],
+    "CSS": ["Borders", "Margins", "Backgrounds", "Float"],
+    "JavaScript": ["Variables", "Operators", "Functions", "Conditions"]    
+  },
+  "Cavite": {
+    "PHP": ["Variables", "Strings", "Arrays"],
+    "SQL": ["SELECT", "UPDATE", "DELETE"]
+  }
+}
+window.onload = function() {
+  var subjectSel = document.getElementById("province");
+  var topicSel = document.getElementById("municipality");
+  var chapterSel = document.getElementById("city");
+  for (var x in subjectObject) {
+    subjectSel.options[subjectSel.options.length] = new Option(x, x);
+  }
+  subjectSel.onchange = function() {
+	//empty Chapters- and Topics- dropdowns
+	chapterSel.length = 1;
+	topicSel.length = 1;
+    //display correct values
+    for (var y in subjectObject[this.value]) {
+      topicSel.options[topicSel.options.length] = new Option(y, y);
+    }
+  }
+  
+  topicSel.onchange = function() {
+	//empty Chapters dropdown
+	chapterSel.length = 1;
+    //display correct values
+    var z = subjectObject[subjectSel.value][this.value];
+    for (var i = 0; i < z.length; i++) {
+      chapterSel.options[chapterSel.options.length] = new Option(z[i], z[i]);
+    }
+  }
+}
+</script>
+
+
 </div>
 </div>
 </body>
