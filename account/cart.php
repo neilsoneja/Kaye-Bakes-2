29b02fe63194c2a0985f2f55cb53c3fd9a1ff2ad
NatesDGreat <88729148+NatesDGreat@users.noreply.github.com>
2022-05-28 16:40:00 +0800

    Css

--- account/cart.php
+++ account/cart.php
@@ -9,7 +9,8 @@ $conn = mysqli_connect($dbservername,$dbUsername,$dbPassword,$dbName);
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="generator" content="Hugo 0.98.0">
@@ -18,55 +19,55 @@ $conn = mysqli_connect($dbservername,$dbUsername,$dbPassword,$dbName);
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/dashboard/">
     <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-    <link rel="stylesheet" type ="text/css" href="css/custom.min.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.min.css">
 
     <style>
-      .bd-placeholder-img {
+    .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
         -webkit-user-select: none;
         -moz-user-select: none;
         user-select: none;
-      }
+    }
 
-      a {
+    a {
         text-decoration: none;
         color: inherit;
-      }
+    }
 
-      @media (min-width: 768px) {
+    @media (min-width: 768px) {
         .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+            font-size: 3.5rem;
         }
-      }
+    }
 
-      .b-example-divider {
+    .b-example-divider {
         height: 3rem;
         background-color: rgba(0, 0, 0, .1);
         border: solid rgba(0, 0, 0, .15);
         border-width: 1px 0;
         box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+    }
 
-      .b-example-vr {
+    .b-example-vr {
         flex-shrink: 0;
         width: 1.5rem;
         height: 100vh;
-      }
+    }
 
-      .bi {
+    .bi {
         vertical-align: -.125em;
         fill: currentColor;
-      }
+    }
 
-      .nav-scroller {
+    .nav-scroller {
         position: relative;
         z-index: 2;
         height: 2.75rem;
         overflow-y: hidden;
-      }
+    }
 
-      .nav-scroller .nav {
+    .nav-scroller .nav {
         display: flex;
         flex-wrap: nowrap;
         padding-bottom: 1rem;
@@ -75,97 +76,95 @@ $conn = mysqli_connect($dbservername,$dbUsername,$dbPassword,$dbName);
         text-align: center;
         white-space: nowrap;
         -webkit-overflow-scrolling: touch;
-      }
+    }
 
-      .bgbrown{
+    .bgbrown {
         background: #31241b;
-      }
+    }
 
-      .bg-lt-brown{
+    .bg-lt-brown {
         background: #423326;
-      }
+    }
 
-      .nav-link {
+    .nav-link {
         font-size: 20px;
-            color: #cbc289;
-        }
- 
-        .nav-item>a:hover {
-            color: #c6b485;
-        }
- 
-        .navbar-nav>.active>a {
-            background-color: #c6c185;
-            color: #E7DA9A;
-        }
-      
+        color: #cbc289;
+    }
 
+    .nav-item>a:hover {
+        color: #c6b485;
+    }
+
+    .navbar-nav>.active>a {
+        background-color: #c6c185;
+        color: #E7DA9A;
+    }
     </style>
-    
+
     <!-- Custom styles for this template -->
     <link href="dashboard.css" rel="stylesheet">
-  </head>
-  <body>
-    
-<header class="navbar navbar-dark sticky-top bgbrown flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="from_menu.php">
-    <img src="images/logo1.png" alt="images/flavicon.png">
-  </a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
-  <div class="navbar-nav">
-    <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="#">Sign out</a>
-    </div>
-  </div>
-</header>
+</head>
 
-<div class="container-fluid">
-  <div class="row">
-    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-lt-brown sidebar collapse">
-      <div class="position-sticky pt-3">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="from_menu.php">
-              <span data-feather="home" class="align-text-bottom"></span>
-              Menu
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="customize-order.php">
-              <span data-feather="home" class="align-text-bottom"></span>
-              Custom Cakes
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="orders.php">
-              <span data-feather="file" class="align-text-bottom"></span>
-              Orders
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="cart.php">
-              <span data-feather="users" class="align-text-bottom"></span>
-              Cart
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
-      </div>
-
-  <div class="main_content">
-    <div class="row">
-      <div class="header">Cart</div> 
-        <div class="text-center">
-            <?php
-
+<body>
+    <header class="navbar navbar-dark sticky-top bgbrown flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="from_menu.php">
+            <img src="images/logo1.png" alt="images/flavicon.png">
+        </a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <input class="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search"
+            aria-label="Search">
+        <div class="navbar-nav">
+            <div class="nav-item text-nowrap">
+                <a class="nav-link px-3" href="#">Sign out</a>
+            </div>
+        </div>
+    </header>
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-lt-brown sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="from_menu.php">
+                                <span data-feather="home" class="align-text-bottom"></span>
+                                Menu
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="customize-order.php">
+                                <span data-feather="home" class="align-text-bottom"></span>
+                                Custom Cakes
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="orders.php">
+                                <span data-feather="file" class="align-text-bottom"></span>
+                                Orders
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="cart.php">
+                                <span data-feather="users" class="align-text-bottom"></span>
+                                Cart
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div
+                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Dashboard</h1>
+                </div>
+                <div class="main_content">
+                    <div class="row">
+                        <div class="header">Cart</div>
+                        <div class="text-center">
+                            <?php
                 $total = 0;
                 $cart_items = array('');
                 $output = "";
@@ -181,14 +180,11 @@ $conn = mysqli_connect($dbservername,$dbUsername,$dbPassword,$dbName);
                             <th>Total Price</th>
                             <th>Dedications</th>
                             <th>Action</th>
-
                         </tr>
                     ";
 
                     if (!empty($_SESSION['cart'])) {
-
                         foreach ($_SESSION['cart'] as $key => $value) {
-
                             $output .= "
                                 <tr>
                                     <td>".$value['product_id']."</td>
@@ -209,7 +205,7 @@ $conn = mysqli_connect($dbservername,$dbUsername,$dbPassword,$dbName);
                                 </tr>
                             ";
                             $total = $total + $value['quantity'] * $value['price'];
-
+                            $all_cart_items = $value['product_id'];
                         }
 
                         $output .= "
@@ -217,6 +213,7 @@ $conn = mysqli_connect($dbservername,$dbUsername,$dbPassword,$dbName);
                           <td colspan='3'></td>
                           <td></b>Total Price</b></td>
                           <td>".number_format($total)."</td>
+                          <td>".$all_cart_items."</td>
                           <td>
                             <a href='from_menu.php?action=clear_all'>
                             <button>Clear All</button>
@@ -226,28 +223,30 @@ $conn = mysqli_connect($dbservername,$dbUsername,$dbPassword,$dbName);
                           </td>
                         </tr>
                         ";
+                    }
 
-                                          ?>
+                  echo $output
+                  ?>
+
                             <div class="check-out">
-                            <form action="post" action="routers/order-router-2.php?product_id=<?=$value['product_id']?>">
-                            <input type="hidden" name="product_name" value="<?= $value['product_name']?>">
-                            <input type="hidden" name="product_desc" value="<?= $value['product_desc']?>">
-                            <input type="submit" name="check-out" value="Proceed to checkout">
-                            </form>
+                                <form method="post"
+                                    action="routers/order-router-2.php?product_id=<?=$value['product_id']?>">
+                                    <input type="hidden" name="product_name" value="<?= $value['product_name']?>">
+                                    <input type="hidden" name="product_desc" value="<?= $value['product_desc']?>">
+                                    <input type="hidden" name="total-price" value="<?= $value ?>">
+                                    <input type="submit" name="check-out" value="Proceed to checkout">
+                                </form>
                             </div>
                             <?php
 
-                    }
-                  echo $output
-
                   ?>
-                  </form>
+                            </form>
+                        </div>
+                    </div>
+                </div>
         </div>
-      </div> 
-    </div>        
-  </div>
 
-  <?php
+        <?php
     if(isset($_GET['action'])) {
 
       if ($_GET['action'] == "clear_all") {
@@ -265,4 +264,5 @@ $conn = mysqli_connect($dbservername,$dbUsername,$dbPassword,$dbName);
   ?>
 
 </body>
+
 </html>
